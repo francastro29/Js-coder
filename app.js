700583223a445909ddbbf2b499cfd3cc07588737
Francisco Castro <castrojavierftuc@gmail.com>
2024-01-05 20:04:44 -0300

    entrega final

--- app.js
+++ app.js
@@ -1,87 +1,193 @@
+var carritoVisible = false;
 
-// Bienvenida 
-alert("¡Bienvenido/a a la tienda de repuestos de Francisco Castro!");
 
-// Nombre
-let fullName = prompt("Ingrese su nombre completo");
-
-// Saludo
-alert(`Bienvenido/a ${fullName} a nuestra tienda`);
-
-const prodTienda = [
-    { producto: 1, precio: 1200000 },
-    { producto: 2, precio: 1350000 },
-    { producto: 3, precio: 2000000 },
-    { producto: 4, precio: 285000 }
-];
-
-let infoProdTienda = "Información sobre la tienda:\n";
-
-for (const prod of prodTienda) {
-    infoProdTienda += `Producto: ${prod.producto} - Precio: ${prod.precio} pesos\n`;
-}
-alert(infoProdTienda);
-
-// Funciones
-function mostrarProdTienda() {
-    let mensaje = "Productos Disponibles:\n";
-
-    for (let i = 0; i < prodTienda.length; i++) {
-    mensaje += `${prodTienda[i].producto} - ${prodTienda[i].precio} pesos\n`;
-    } 
-
-    alert(mensaje);
+if(document.readyState == 'loading'){
+    document.addEventListener('DOMContentLoaded', ready)
+}else{
+    ready();
 }
 
-function encontrarProdPorNombre(producto) {
-    const prodEncontrado = prodTienda.find(prod => prod.producto === producto);
+function ready(){
+    
 
-    if (prodEncontrado) {
-    return prodEncontrado;
-    } else {
-    return null;
+    var botonesEliminarItem = document.getElementsByClassName('btn-eliminar');
+    for(var i=0;i<botonesEliminarItem.length; i++){
+        var button = botonesEliminarItem[i];
+        button.addEventListener('click',eliminarItemCarrito);
+    }
+
+
+    var botonesSumarCantidad = document.getElementsByClassName('sumar-cantidad');
+    for(var i=0;i<botonesSumarCantidad.length; i++){
+        var button = botonesSumarCantidad[i];
+        button.addEventListener('click',sumarCantidad);
+    }
+
+
+    var botonesRestarCantidad = document.getElementsByClassName('restar-cantidad');
+    for(var i=0;i<botonesRestarCantidad.length; i++){
+        var button = botonesRestarCantidad[i];
+        button.addEventListener('click',restarCantidad);
+    }
+
+    var botonesAgregarAlCarrito = document.getElementsByClassName('boton-item');
+    for(var i=0; i<botonesAgregarAlCarrito.length;i++){
+        var button = botonesAgregarAlCarrito[i];
+        button.addEventListener('click', agregarAlCarritoClicked);
+    }
+
+
+    document.getElementsByClassName('btn-pagar')[0].addEventListener('click',pagarClicked)
+}
+
+function pagarClicked(){
+    alert("Gracias por la compra");
+
+    var carritoItems = document.getElementsByClassName('carrito-items')[0];
+    while (carritoItems.hasChildNodes()){
+        carritoItems.removeChild(carritoItems.firstChild)
+    }
+    actualizarTotalCarrito();
+    ocultarCarrito();
+}
+
+function agregarAlCarritoClicked(event){
+    var button = event.target;
+    var item = button.parentElement;
+    var titulo = item.getElementsByClassName('titulo-item')[0].innerText;
+    var precio = item.getElementsByClassName('precio-item')[0].innerText;
+    var imagenSrc = item.getElementsByClassName('img-item')[0].src;
+    console.log(imagenSrc);
+
+    agregarItemAlCarrito(titulo, precio, imagenSrc);
+
+    hacerVisibleCarrito();
+}
+
+
+function hacerVisibleCarrito(){
+    carritoVisible = true;
+    var carrito = document.getElementsByClassName('carrito')[0];
+    carrito.style.marginRight = '0';
+    carrito.style.opacity = '1';
+
+    var items =document.getElementsByClassName('contenedor-items')[0];
+    items.style.width = '60%';
+}
+
+
+function agregarItemAlCarrito(titulo, precio, imagenSrc){
+    var item = document.createElement('div');
+    item.classList.add = ('item');
+    var itemsCarrito = document.getElementsByClassName('carrito-items')[0];
+
+
+    var nombresItemsCarrito = itemsCarrito.getElementsByClassName('carrito-item-titulo');
+    for(var i=0;i < nombresItemsCarrito.length;i++){
+        if(nombresItemsCarrito[i].innerText==titulo){
+            alert("El item ya se encuentra en el carrito");
+            return;
+        }
+    }
+
+    var itemCarritoContenido = `
+        <div class="carrito-item">
+            <img src="${imagenSrc}" width="80px" alt="">
+            <div class="carrito-item-detalles">
+                <span class="carrito-item-titulo">${titulo}</span>
+                <div class="selector-cantidad">
+                    <i class="fa-solid fa-minus restar-cantidad"></i>
+                    <input type="text" value="1" class="carrito-item-cantidad" disabled>
+                    <i class="fa-solid fa-plus sumar-cantidad"></i>
+                </div>
+                <span class="carrito-item-precio">${precio}</span>
+            </div>
+            <button class="btn-eliminar">
+                <i class="fa-solid fa-trash"></i>
+            </button>
+        </div>
+    `
+    item.innerHTML = itemCarritoContenido;
+    itemsCarrito.append(item);
+
+
+     item.getElementsByClassName('btn-eliminar')[0].addEventListener('click', eliminarItemCarrito);
+
+    var botonRestarCantidad = item.getElementsByClassName('restar-cantidad')[0];
+    botonRestarCantidad.addEventListener('click',restarCantidad);
+
+
+    var botonSumarCantidad = item.getElementsByClassName('sumar-cantidad')[0];
+    botonSumarCantidad.addEventListener('click',sumarCantidad);
+
+
+    actualizarTotalCarrito();
+}
+
+function sumarCantidad(event){
+    var buttonClicked = event.target;
+    var selector = buttonClicked.parentElement;
+    console.log(selector.getElementsByClassName('carrito-item-cantidad')[0].value);
+    var cantidadActual = selector.getElementsByClassName('carrito-item-cantidad')[0].value;
+    cantidadActual++;
+    selector.getElementsByClassName('carrito-item-cantidad')[0].value = cantidadActual;
+    actualizarTotalCarrito();
+}
+
+function restarCantidad(event){
+    var buttonClicked = event.target;
+    var selector = buttonClicked.parentElement;
+    console.log(selector.getElementsByClassName('carrito-item-cantidad')[0].value);
+    var cantidadActual = selector.getElementsByClassName('carrito-item-cantidad')[0].value;
+    cantidadActual--;
+    if(cantidadActual>=1){
+        selector.getElementsByClassName('carrito-item-cantidad')[0].value = cantidadActual;
+        actualizarTotalCarrito();
     }
 }
 
-function filtrarProdPorPrecio(min, max) {
-    const prodFiltrados = prodTienda.filter(prod => prod.precio >= min && prod.precio <= max);
-    return prodFiltrados;
+function eliminarItemCarrito(event){
+    var buttonClicked = event.target;
+    buttonClicked.parentElement.parentElement.remove();
+
+    actualizarTotalCarrito();
+
+    //la siguiente funciòn controla si hay elementos en el carrito
+    //Si no hay elimino el carrito
+    ocultarCarrito();
 }
-
-// Entrada de datos del cliente
-const prodCliente = parseFloat(prompt("1: Horquilla, 2:Suspension, 3:Transmision, 4:Cuadro, Ingrese el numero del producto que desea comprar:"));
-
-// Búsqueda de producto
-const prodElegido = encontrarProdPorNombre(prodCliente);
-
-// Mostrar resultados
-if (prodElegido) {
-    const precioProd = prodElegido.precio ;
-    alert(`Has elegido un/una ${prodElegido.producto}. El costo es de ${precioProd.toFixed(2)} pesos.`);
-} else {
-    alert("Lo siento, no tenemos ese producto disponible.");
-}
-
-// Otra compra
-let otraCompra = prompt("¿Desea comprar algo más? (S/N)").toLowerCase();
-
-if (otraCompra === "S") {
-  mostrarProdTienda(); // Mostrar productos disponibles
-
-  // Entrada de datos del cliente para la nueva compra
-    const prodClienteNuevo = parseFloat(prompt("Ingresa el nombre del producto deseado para tu nueva compra:"));
-
-  // Búsqueda de pesa rusa para la nueva compra
-    const prodElegidoNuevo = encontrarProdPorNombre(prodClienteNuevo);
-
-  // Mostrar resultados para la nueva compra
-    if (prodElegidoNuevo) {
-    const precioNuevo = prodElegidoNuevo.precio ; 
-    alert(`Has elegido un/una ${pesaElegidaNueva.peso}. El costo es de ${precioNuevo.toFixed(2)} pesos.`);
-    alert("Gracias por comprar.");
-    } else {
-    alert("Lo siento, no tenemos ese producto disponible.");
+//Funciòn que controla si hay elementos en el carrito. Si no hay oculto el carrito.
+function ocultarCarrito(){
+    var carritoItems = document.getElementsByClassName('carrito-items')[0];
+    if(carritoItems.childElementCount==0){
+        var carrito = document.getElementsByClassName('carrito')[0];
+        carrito.style.marginRight = '-100%';
+        carrito.style.opacity = '0';
+        carritoVisible = false;
+    
+        var items =document.getElementsByClassName('contenedor-items')[0];
+        items.style.width = '100%';
     }
-}   else {
-    alert("Gracias por comprar.");
+}
+
+function actualizarTotalCarrito(){
+ 
+    var carritoContenedor = document.getElementsByClassName('carrito')[0];
+    var carritoItems = carritoContenedor.getElementsByClassName('carrito-item');
+    var total = 0;
+
+    for(var i=0; i< carritoItems.length;i++){
+        var item = carritoItems[i];
+        var precioElemento = item.getElementsByClassName('carrito-item-precio')[0];
+
+        var precio = parseFloat(precioElemento.innerText.replace('$','').replace('.',''));
+        var cantidadItem = item.getElementsByClassName('carrito-item-cantidad')[0];
+        console.log(precio);
+        var cantidad = cantidadItem.value;
+        total = total + (precio * cantidad);
+    }
+    total = Math.round(total * 100)/100;
+
+    document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
+
 }
